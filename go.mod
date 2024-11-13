c7ec9c12d855628cba5deef3fd8846e5011807a1
I Komang Setiana <komangsetiana40@gmail.com>
2024-11-13 21:58:09 +0800

    menggunakan modul say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module KomangSetiana/app-helo-go
+
+go 1.19
+
+require github.com/KomangSetiana/hello-go v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/KomangSetiana/hello-go"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
